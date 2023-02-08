8950ad10362f9931cc3335fa37f41d7770a12b94
Ederson Silva <edsjcbra@gmail.com>
2023-02-08 14:31:44 +0000

    Added Folder Model and Class AddTicket

---
+++ Eticket/Models/AddTicket.cs
@@ -0,0 +1,10 @@
+﻿namespace Eticket.Models
+{
+    public class AddTicket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double  Price { get; set; }
+        public DateTime TravelDate { get; set; }
+    }
+}
