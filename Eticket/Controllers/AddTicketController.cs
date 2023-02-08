63a0e01561bc69564ebca58759098948073569a9
Ederson Silva <edsjcbra@gmail.com>
2023-02-08 14:21:20 +0000

    Added AddTicket Controller

---
+++ Eticket/Controllers/AddTicketController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eticket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddTicketController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddTicket(AddTicketInput input)
+        {
+
+            return Created("", input);
+        }
+    }
+}
